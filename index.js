bdc2b4bd71fb2ad47b5005dcb67aa5ab19e2105e
Mark <mark@murphymark.me>
2022-03-05 09:52:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,484 @@
+let noslugs = [
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/checkbox',
+    title: 'checkbox',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/color',
+    title: 'color',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/font',
+    title: 'font',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/heading',
+    title: 'heading',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/imageDimension',
+    title: 'imageDimension',
+  },
+  {
+    href:
+      '/stencil-docs/reference-docs/styles-and-properties/optimizedCheckout-image',
+    title: 'optimizedCheckout-image',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/reference',
+    title: 'reference',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/select',
+    title: 'select',
+  },
+  {
+    href: '/stencil-docs/reference-docs/styles-and-properties/text',
+    title: 'text',
+  },
+];
+
+let withslugs = [
+  {
+    id: 'c2NoOjM0NzUzMjY4',
+    title: 'Alignment',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjY4-alignment',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjY3',
+    title: 'Boolean',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjY3-boolean',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjY2',
+    title: 'Box Model',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjY2-box-model',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjY1',
+    title: 'Code',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjY1-code',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjY0',
+    title: 'Color',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjY0-color',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjYz',
+    title: 'Element Settings',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjYz-element-settings',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjYy',
+    title: 'Image Manager',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjYy-image-manager',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjYx',
+    title: 'Input',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjYx-input',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjYw',
+    title: 'Number',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjYw-number',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU5',
+    title: 'Product ID',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU5-product-id',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU4',
+    title: 'Product Image',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU4-product-image',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NjczNDk3',
+    title: 'Range',
+    type: 'model',
+    slug: 'c2NoOjM0NjczNDk3-range',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjUz',
+    title: 'Regex Input',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjUz-regex-input',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU0',
+    title: 'Select',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU0-select',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU1',
+    title: 'Text',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU1-text',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU2',
+    title: 'Typography',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU2-typography',
+    meta: '',
+  },
+  {
+    id: 'c2NoOjM0NzUzMjU3',
+    title: 'Visibility',
+    type: 'model',
+    slug: 'c2NoOjM0NzUzMjU3-visibility',
+    meta: '',
+  },
+];
+
+const raw = {
+  items: [
+    {
+      type: 'divider',
+      title: 'APIS',
+    },
+    {
+      type: 'divider',
+      title: 'Schemas',
+    },
+    {
+      type: 'item',
+      title: 'banners.yml',
+      uri: 'models/_root/banners.yml',
+    },
+    {
+      type: 'item',
+      title: 'blog.yml',
+      uri: 'models/_root/blog.yml',
+    },
+    {
+      type: 'item',
+      title: 'brand.yml',
+      uri: 'models/_root/brand.yml',
+    },
+    {
+      type: 'item',
+      title: 'brand_results',
+      uri: 'models/_root/brand_results.yaml',
+    },
+    {
+      type: 'item',
+      title: 'brands',
+      uri: 'models/_root/brands.yml',
+    },
+    {
+      type: 'item',
+      title: 'breadcrumbs.yml',
+      uri: 'models/_root/breadcrumbs.yml',
+    },
+    {
+      type: 'item',
+      title: 'cart.yml',
+      uri: 'models/_root/cart.yml',
+    },
+    {
+      type: 'item',
+      title: 'cart_id.yml',
+      uri: 'models/_root/cart_id.yml',
+    },
+    {
+      type: 'item',
+      title: 'categories.yml',
+      uri: 'models/_root/categories.yml',
+    },
+    {
+      type: 'item',
+      title: 'category',
+      uri: 'models/_root/category.yml',
+    },
+    {
+      type: 'item',
+      title: 'category_results',
+      uri: 'models/_root/category_results.yaml',
+    },
+    {
+      type: 'item',
+      title: 'checkout.yml',
+      uri: 'models/_root/checkout.yml',
+    },
+    {
+      type: 'item',
+      title: 'comparisons',
+      uri: 'models/_root/comparisons.yml',
+    },
+    {
+      type: 'item',
+      title: 'content_results',
+      uri: 'models/_root/content_results.yaml',
+    },
+    {
+      type: 'item',
+      title: 'countries.yml',
+      uri: 'models/_root/countries.yml',
+    },
+    {
+      type: 'item',
+      title: 'create_account.yml',
+      uri: 'models/_root/create_account.yml',
+    },
+    {
+      type: 'item',
+      title: 'currency_selector.yml',
+      uri: 'models/_root/currency_selector.yml',
+    },
+    {
+      type: 'item',
+      title: 'customer.yml',
+      uri: 'models/_root/customer.yml',
+    },
+    {
+      type: 'item',
+      title: 'customer_group_id.yml',
+      uri: 'models/_root/customer_group_id.yml',
+    },
+    {
+      type: 'item',
+      title: 'customer_group_name.yml',
+      uri: 'models/_root/customer_group_name.yml',
+    },
+    {
+      type: 'item',
+      title: 'downloads.yml',
+      uri: 'models/_root/downloads.yml',
+    },
+    {
+      type: 'item',
+      title: 'faceted_search.yml',
+      uri: 'models/_root/faceted_search.yml',
+    },
+    {
+      type: 'item',
+      title: 'footer',
+      uri: 'models/_root/footer.yml',
+    },
+    {
+      type: 'item',
+      title: 'forms.yml',
+      uri: 'models/_root/forms.yml',
+    },
+    {
+      type: 'item',
+      title: 'head.yml',
+      uri: 'models/_root/head.yml',
+    },
+    {
+      type: 'item',
+      title: 'is_ajax.yml',
+      uri: 'models/_root/is_ajax.yml',
+    },
+    {
+      type: 'item',
+      title: 'order.yml',
+      uri: 'models/_root/order.yml',
+    },
+    {
+      type: 'item',
+      title: 'page.yml',
+      uri: 'models/_root/page.yml',
+    },
+    {
+      type: 'item',
+      title: 'page_type.yml',
+      uri: 'models/_root/page_type.yml',
+    },
+    {
+      type: 'item',
+      title: 'pages.yml',
+      uri: 'models/_root/pages.yml',
+    },
+    {
+      type: 'item',
+      title: 'product.yml',
+      uri: 'models/_root/product.yml',
+    },
+    {
+      type: 'item',
+      title: 'product_results',
+      uri: 'models/_root/product_results.yaml',
+    },
+    {
+      type: 'item',
+      title: 'products.yml',
+      uri: 'models/_root/products.yml',
+    },
+    {
+      type: 'item',
+      title: 'remote_api_scripts.yml',
+      uri: 'models/_root/remote_api_scripts.yml',
+    },
+    {
+      type: 'item',
+      title: 'settings',
+      uri: 'models/_root/settings.yml',
+    },
+    {
+      type: 'item',
+      title: 'shipments.yml',
+      uri: 'models/_root/shipments.yml',
+    },
+    {
+      type: 'item',
+      title: 'shipping_messages.yml',
+      uri: 'models/_root/shipping_messages.yml',
+    },
+    {
+      type: 'item',
+      title: 'shop_by_brand',
+      uri: 'models/_root/shop_by_brand.yml',
+    },
+    {
+      type: 'item',
+      title: 'sitemap.yml',
+      uri: 'models/_root/sitemap.yml',
+    },
+    {
+      type: 'item',
+      title: 'social_media.yml',
+      uri: 'models/_root/social_media.yml',
+    },
+    {
+      type: 'item',
+      title: 'template_file.yml',
+      uri: 'models/_root/template_file.yml',
+    },
+    {
+      type: 'item',
+      title: 'theme_settings.yml',
+      uri: 'models/_root/theme_settings.yml',
+    },
+    {
+      type: 'item',
+      title: 'vault.yml',
+      uri: 'models/_root/vault.yml',
+    },
+    {
+      type: 'item',
+      title: 'carousel.yml',
+      uri: 'models/_root/carousel.yml',
+    },
+    {
+      type: 'item',
+      title: 'pagination.yml',
+      uri: 'models/_root/pagination.yml',
+    },
+    {
+      type: 'item',
+      title: 'urls.yml',
+      uri: 'models/_root/urls.yml',
+    },
+    {
+      type: 'item',
+      title: 'wishlist.yml',
+      uri: 'models/_root/wishlist.yml',
+    },
+  ],
+};
+
+
+// function joinThese() {
+//   for (const item in withslugs) {
+//     Object.assign(noslugs[item], withslugs[item]);
+//   }
+//   return noslugs;
+// }
+
+async function mergeTags(toc) {
+  function copy(o) {
+    return Object.assign({}, o);
+  }
+
+  
+  let newArr = [];
+  async function printAllVals(obj) {
+    for (let k in obj) {
+      // let nodeData = await getNodeData(obj.slug?.split('-')[0]);
+      if (typeof obj[k] === 'object') {
+        await printAllVals(obj[k]);
+      } else {
+        // let newObj = {
+        //   ...obj,
+        //   nodeData,
+        // };
+
+        if (k == 'uri') {
+          // console.log(obj[k]);
+          // newArr.push(obj[k])
+          console.log(obj[k])
+          return obj[k]
+          // console.log(obj[k]);
+        }
+      }
+    }
+    return obj;
+  }
+
+  let resolve = copy(toc);
+  // console.log(resolve)
+  // let tocItemsCopy = resolve.duplicates.map(copy);
+  let resItems = await printAllVals(resolve);
+  return resItems;
+  // console.log(resItems)
+}
+
+//  function NumberList(props) {
+//    const titles = props.titles;
+//    let titlesArr = [];
+
+//    function printAllVals(obj) {
+//      for (let k in obj) {
+//        if (typeof obj[k] === 'object') {
+//          printAllVals(obj[k]);
+//        } else {
+//          k == 'title' ? titlesArr.push(obj[k]) : null;
+//        }
+//      }
+//      return titlesArr;
+//    }
+
+//    function removeDupes(array) {
+//      return array.filter((value, index) => array.indexOf(value) === index);
+//    }
+
+//    const listItems = removeDupes(printAllVals(titles)).map((title, index) => (
+//      <ListItem key={index} value={title} />
+//    ));
+
+//    return <ul>{listItems}</ul>;
+//  }
+console.log(mergeTags(raw))
+
+// const results = mergeTags(raw);
